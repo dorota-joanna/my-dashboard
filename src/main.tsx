d3a1bd7a5997b868dd7af0bf696d3e273acdaf1d
Saphire Pencil <saphirepencil@Saphires-MacBook-Pro.local>
2026-03-04 15:49:03 +0000

    render root element only if exists

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,13 @@ import ReactDOM from 'react-dom/client'
 import App from './app/App'
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
+
+ReactDOM.createRoot(rootElement).render(
   
   <React.StrictMode>
     <BrowserRouter>
